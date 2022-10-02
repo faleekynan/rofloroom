b8a149fb10957ff9f6f0acb5a3366cdfe22156cd
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-10-02 14:27:47 +0300

    Add files via upload

---
+++ fp/posts/script.js
@@ -0,0 +1,26 @@
+var randomQuote = "";
+var randomAuthor = "";
+
+function getQuote() {
+  $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+      method: "GET",
+      dataType: "jsonp",
+      success: function(request) {
+        randomQuote = request.quoteText;
+        randomAuthor = request.quoteAuthor;
+        $('#text').html(randomQuote);
+        if (randomAuthor === "")        { randomAuthor="Unknown";
+        }    $('#author').html(randomAuthor);
+ },
+      error: function(xhr, status, error) 
+{
+    $('#quoteText').text('Not sure what happened there! Click again to generate a new quote!');
+        $('#quoteAuthor').text('Click Below!');
+}
+  });
+}
+  
+
+
+      getQuote();
