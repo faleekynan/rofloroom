8b30b3d8410ae666a4342b8ad31cc79c5dc5765a
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-10-07 16:15:30 +0300

    Add files via upload

---
+++ doc_gen/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', e => {
+  let file = document.querySelector('#file'),        // Выбираем нужные
+      preview = document.querySelector('#preview');  // элементы
+  
+  file.addEventListener('change', e => { // При изменении input
+    if(file.files.length === 0) // Если ничего не выбрано - выходим
+      return;
+    
+    let f = file.files[0],     // Берём первый файл
+        fr = new FileReader(); // Создаём объект чтения файлов
+    
+    // В свойсте type mime (что-то типа image/png)
+    if(f.type.indexOf('image') === -1) // Если файл не является изображением - выходим
+      return;
+    
+    fr.onload = e => {
+      if(getComputedStyle(preview, null).display === 'none') // Если нужно - показываем img
+        preview.style.display = 'block';
+      
+      preview.src = e.target.result; // В src будет что-то типа data:image/jpeg;base64,....
+    }
+    fr.readAsDataURL(f); // Читаем blob выбранного файла
+  });
+});
