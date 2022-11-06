e0a33ddacf7b4f0ae17f20f3b25c6a4b130c1c9e
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-11-07 01:33:12 +0200

    перевод

---
+++ doc_gen/copy.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  let fn_ua = document.querySelector('.fn_ua');
+  let fn_en = document.querySelector('.fn_en');
+  let ln_ua = document.querySelector('.ln_ua');
+  let ln_en = document.querySelector('.ln_en');
+  
+  fn_en.addEventListener('input', function() {
+    fn_ua.value = translit(this.value);
+  });
+  fn_ua.addEventListener('input', function() {
+    fn_en.value = translit1(this.value);
+  });
+  
+  ln_en.addEventListener('input', function() {
+    ln_ua.value = translit(this.value);
+  });
+  ln_ua.addEventListener('input', function() {
+    ln_en.value = translit1(this.value);
+  });
+  
+  
+};
