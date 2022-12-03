fc8941a6cb46700c8933a3fcfdaccc09a3413082
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-12-03 16:57:32 +0200

    поля ввода

--- doc_gen/copy.js
+++ doc_gen/copy.js
@@ -1,30 +1,44 @@
 window.onload = function() {
-  let fn_ua = document.querySelector('.fn_ua');
-  let fn_en = document.querySelector('.fn_en');
-  let ln_ua = document.querySelector('.ln_ua');
-  let ln_en = document.querySelector('.ln_en');
-  
-  var src = document.getElementById("birth"),
-        dst = document.getElementById("rec");
-  
-  fn_en.addEventListener('input', function() {
-    fn_ua.value = translit(this.value);
-  });
-  fn_ua.addEventListener('input', function() {
-    fn_en.value = translit1(this.value);
-  });
-  
-  ln_en.addEventListener('input', function() {
-    ln_ua.value = translit(this.value);
-  });
-  ln_ua.addEventListener('input', function() {
-    ln_en.value = translit1(this.value);
-  });
-  
-  
-    src.addEventListener('input', function() {
-        dst.value = src.value;
+  var src = document.getElementById("main_row"),
+      date = document.getElementById("date");
+        en = document.getElementById("en");
+       ru = document.getElementById("ru");
+       rec = document.getElementById("record_row");
+  src.addEventListener('input', function() {
+	  date.value = src.value;
+        en.value = src.value;
+        ru.value = src.value;
+        rec.value = src.value;
     });
+  
+  main_row.addEventListener('input', function() {
+    en.value = translitRuEn(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    ru.value = translitRuRu(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    date.value = translitRu0(this.value);
+  });
+  main_row.addEventListener('input', function() {
+    rec.value = translitRu0(this.value);
+  });
+  
   
   
 };
+
+document.getElementById('main_row').addEventListener('input', function(e){
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split('	').join('\n');
+    e.target.value = e.target.value.split('	').join('\n');
+	e.target.value = e.target.value.split(' ').join('\n');
+});
+
+    
+main_row.addEventListener('input', function() {
+    record_row.value = translit2(this.value);
+  });
--- doc_gen/middle_name.js
+++ doc_gen/middle_name.js
@@ -1,21 +1,21 @@
 pic = new Array(
 
-'<input onfocus="this.select()" value = "Олександрівна">',
-'<input onfocus="this.select()" value = "Олегівна">',
-'<input onfocus="this.select()" value = "Максимівна">',
-'<input onfocus="this.select()" value = "Дмитрівна">',
-'<input onfocus="this.select()" value = "Павлівна">',
-'<input onfocus="this.select()" value = "Владиславівна">',
-'<input onfocus="this.select()" value = "Володимирівна">',
-'<input onfocus="this.select()" value = "Сергіївна">',
-'<input onfocus="this.select()" value = "Євгенівна">',
-'<input onfocus="this.select()" value = "Юріївна">',
-'<input onfocus="this.select()" value = "Олексіївна">',
-'<input onfocus="this.select()" value = "Ілівна">',
-'<input onfocus="this.select()" value = "Іванівна">',
-'<input onfocus="this.select()" value = "Василівна">',
-'<input onfocus="this.select()" value = "Віталіївна">',
-'<input onfocus="this.select()" value = "Петрівна">'
+'<textarea onfocus="this.select()" value = "">Олександрівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Олегівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Максимівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Дмитрівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Павлівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Владиславівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Володимирівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Сергіївна</textarea>',
+'<textarea onfocus="this.select()" value = "">Євгенівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Юріївна</textarea>',
+'<textarea onfocus="this.select()" value = "">Олексіївна</textarea>',
+'<textarea onfocus="this.select()" value = "">Ілівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Іванівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Василівна</textarea>',
+'<textarea onfocus="this.select()" value = "">Віталіївна</textarea>',
+'<textarea onfocus="this.select()" value = "">Петрівна</textarea>'
                         );
                         n = Math.floor(Math.random() * 16);
                         document.write(pic[n]);
