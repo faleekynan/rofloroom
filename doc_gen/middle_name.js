82d20be65fea19c4e133f17f8d6b07280e43df83
faleekynan <73445708+faleekynan@users.noreply.github.com>
2022-12-04 12:30:47 +0200

    размеры

--- doc_gen/middle_name.js
+++ doc_gen/middle_name.js
@@ -1,21 +1,21 @@
 pic = new Array(
 
-'<textarea onfocus="this.select()" value = "">Олександрівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Олегівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Максимівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Дмитрівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Павлівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Владиславівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Володимирівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Сергіївна</textarea>',
-'<textarea onfocus="this.select()" value = "">Євгенівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Юріївна</textarea>',
-'<textarea onfocus="this.select()" value = "">Олексіївна</textarea>',
-'<textarea onfocus="this.select()" value = "">Ілівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Іванівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Василівна</textarea>',
-'<textarea onfocus="this.select()" value = "">Віталіївна</textarea>',
-'<textarea onfocus="this.select()" value = "">Петрівна</textarea>'
+'<textarea readonly >Олександрівна</textarea>',
+'<textarea readonly >Олегівна</textarea>',
+'<textarea readonly >Максимівна</textarea>',
+'<textarea readonly >Дмитрівна</textarea>',
+'<textarea readonly >Павлівна</textarea>',
+'<textarea readonly >Владиславівна</textarea>',
+'<textarea readonly >Володимирівна</textarea>',
+'<textarea readonly >Сергіївна</textarea>',
+'<textarea readonly >Євгенівна</textarea>',
+'<textarea readonly >Юріївна</textarea>',
+'<textarea readonly >Олексіївна</textarea>',
+'<textarea readonly >Ілівна</textarea>',
+'<textarea readonly >Іванівна</textarea>',
+'<textarea readonly >Василівна</textarea>',
+'<textarea readonly >Віталіївна</textarea>',
+'<textarea readonly >Петрівна</textarea>'
                         );
                         n = Math.floor(Math.random() * 16);
                         document.write(pic[n]);
